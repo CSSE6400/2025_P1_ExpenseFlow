ba5bc7ab36fb575c69a1e790e6f0c53416522f6a
aadityayadav17 <yadavaadi17@icloud.com>
2025-05-20 10:54:02 +1000

    Home Screen (7/n) - Recent Expenses Widget

--- ui/lib/screens/home_screen/elements/home_screen_recent_expenses.dart
+++ ui/lib/screens/home_screen/elements/home_screen_recent_expenses.dart
@@ -4,25 +4,108 @@ import 'package:flutter/material.dart';
 import '../../../common/color_palette.dart';
 import '../../../common/proportional_sizes.dart';
 
-class HomeScreenRecentExpenses extends StatelessWidget {
+class RecentExpenseItem {
+  final String name;
+  final String price;
+
+  RecentExpenseItem({
+    required this.name,
+    required this.price,
+  });
+}
+
+class HomeScreenRecentExpenses extends StatefulWidget {
   const HomeScreenRecentExpenses({super.key});
+
+  @override
+  State<HomeScreenRecentExpenses> createState() => _HomeScreenRecentExpensesState();
+}
+
+class _HomeScreenRecentExpensesState extends State<HomeScreenRecentExpenses> {
+  late final List<RecentExpenseItem> recentExpenses;
+
+  @override
+  void initState() {
+    super.initState();
+    // Sample data for recent expenses
+    // TODO: Replace with actual data from your database. Fetch 6 most recent expenses.
+    recentExpenses = [
+      RecentExpenseItem(name: 'Shopping at Coles', price: '78.9'),
+      RecentExpenseItem(name: 'Uber Ride', price: '25.5'),
+      RecentExpenseItem(name: 'Dinner at Sushi Train', price: '60.2'),
+      RecentExpenseItem(name: 'Movie Tickets', price: '34'),
+      RecentExpenseItem(name: 'Fuel BP Station', price: '89.9'),
+      RecentExpenseItem(name: 'Haircut', price: '45'),
+    ];
+  }
+
+  formatPrice(String price) {
+    return '\$${double.parse(price).toStringAsFixed(2)}';
+  }
+
 
   @override
   Widget build(BuildContext context) {
     final proportionalSizes = ProportionalSizes(context: context);
     final backgroundColor = ColorPalette.buttonText;
 
-    return Container(
-      width: double.infinity,
-      height: proportionalSizes.scaleHeight(240),
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        borderRadius: BorderRadius.circular(
-          proportionalSizes.scaleWidth(10),
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/expenses');
+      },
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(
+            proportionalSizes.scaleWidth(10),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(proportionalSizes.scaleWidth(16)),
+              child: Text(
+                'Recent Expenses',
+                style: TextStyle(
+                  fontSize: proportionalSizes.scaleText(24),
+                  fontWeight: FontWeight.bold,
+                  color: ColorPalette.primaryText,
+                ),
+              ),
+            ),
+
+            ...recentExpenses.map(
+              (expense) => Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: proportionalSizes.scaleWidth(16),
+                  vertical: proportionalSizes.scaleHeight(8),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      expense.name,
+                      style: TextStyle(
+                        fontSize: proportionalSizes.scaleText(18),
+                        color: ColorPalette.primaryText,
+                      ),
+                    ),
+                    Text(
+                      formatPrice(expense.price),
+                      style: TextStyle(
+                        fontSize: proportionalSizes.scaleText(18),
+                        color: ColorPalette.primaryText,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]
         ),
       ),
-
-      // TODO: Add functionality to Show Recent Expenses here
     );
   }
 }
