8ef64ac84411939ce80acc84366efe670ea3a568
aadityayadav17 <yadavaadi17@icloud.com>
2025-05-23 19:33:02 +1000

    Groups & Friends (2/n) - Segment Control

--- ui/lib/screens/groups_and_friends_screen/elements/groups_and_friends_main_body.dart
+++ ui/lib/screens/groups_and_friends_screen/elements/groups_and_friends_main_body.dart
@@ -1,32 +1,54 @@
 import 'package:flutter/material.dart';
 // Common imports
 import '../../../common/proportional_sizes.dart';
+import 'groups_and_friends_segment_control.dart';
 
 class GroupsAndFriendsMainBody extends StatefulWidget {
   const GroupsAndFriendsMainBody({super.key});
 
   @override
-  State<GroupsAndFriendsMainBody> createState() => _GroupsAndFriendsMainBodyState();
+  State<GroupsAndFriendsMainBody> createState() =>
+      _GroupsAndFriendsMainBodyState();
 }
 
 class _GroupsAndFriendsMainBodyState extends State<GroupsAndFriendsMainBody> {
+  String selectedSegment = 'Friends';
+
+  void updateSegment(String newSegment) {
+    setState(() {
+      selectedSegment = newSegment;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final proportionalSizes = ProportionalSizes(context: context);
 
     return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).unfocus();
-      },
+      onTap: () => FocusScope.of(context).unfocus(),
       child: SafeArea(
         child: SingleChildScrollView(
           padding: EdgeInsets.symmetric(
             horizontal: proportionalSizes.scaleWidth(20),
-            vertical: proportionalSizes.scaleHeight(20),
+            vertical: proportionalSizes.scaleHeight(0),
           ),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              // Segment Control
+              GroupsAndFriendsSegmentControl(
+                selectedSegment: selectedSegment,
+                onSegmentChanged: updateSegment,
+              ),
+              const SizedBox(height: 12),
+
+              if (selectedSegment == 'Friends') ...[
+                // TODO: Insert Friends View here
+              ]
+
+              else ...[
+                // TODO: Insert Groups View here
+              ],
             ],
           ),
         ),
