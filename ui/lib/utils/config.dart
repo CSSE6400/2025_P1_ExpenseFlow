5da8498e5d14ad86950d85a09b90402a4ab6b46f
Ella Berglas <s4742538@student.uq.edu.au>
2025-06-02 22:29:29 +1000

    put config and plugin stuff back

--- ui/lib/utils/config.dart
+++ ui/lib/utils/config.dart
@@ -16,34 +16,25 @@ class Config {
     required this.jwtAudience,
   });
 
-    static Future<Config> load() async {
-    return Config(
-      backendBaseUrl: "http://localhost:8080",
-      auth0Domain: "expenseflow.au.auth0.com",
-      auth0ClientId: "NU2denF2gSlkVyaOeTxO7c1IgZliJe02",
-      jwtAudience: "https://expenseflow.com/api",
-    );
-  //   final Logger logger = Logger("Config");
-  // ...
-  // static Future<Config> load() async {
-  //   final Logger logger = Logger("Config");
+  static Future<Config> load() async {
+    final Logger logger = Logger("Config");
 
-  //   logger.info("Sending request to /config");
-  //   final response = await http.get(Uri.base.resolve('/config'));
-  //   if (response.statusCode == 200) {
-  //     final json = jsonDecode(response.body) as Map<String, dynamic>;
-  //     logger.config("Got the following from /config: $json");
+    logger.info("Sending request to /config");
+    final response = await http.get(Uri.base.resolve('/config'));
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map<String, dynamic>;
+      logger.config("Got the following from /config: $json");
 
-  //     return Config(
-  //       backendBaseUrl: json['BACKEND_BASE_URL'] as String,
-  //       auth0Domain: json['AUTH0_DOMAIN'] as String,
-  //       auth0ClientId: json['AUTH0_CLIENT_ID'] as String,
-  //       jwtAudience: json['JWT_AUDIENCE'] as String,
-  //     );
-  //   } else {
-  //     throw Exception(
-  //       'Failed to load config ${response.statusCode}: ${response.body}',
-  //     );
-  //   }
+      return Config(
+        backendBaseUrl: json['BACKEND_BASE_URL'] as String,
+        auth0Domain: json['AUTH0_DOMAIN'] as String,
+        auth0ClientId: json['AUTH0_CLIENT_ID'] as String,
+        jwtAudience: json['JWT_AUDIENCE'] as String,
+      );
+    } else {
+      throw Exception(
+        'Failed to load config ${response.statusCode}: ${response.body}',
+      );
     }
+  }
 }
