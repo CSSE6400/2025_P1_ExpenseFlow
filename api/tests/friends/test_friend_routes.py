61a97d8af1e6e87cd28f61219371de65e47515d3
Donghyug David Jeong <donghyug.jeong@student.uq.edu.au>
2025-06-03 18:33:46 +1000

    Remove unused imports

--- api/tests/friends/test_friend_routes.py
+++ api/tests/friends/test_friend_routes.py
@@ -2,22 +2,15 @@
 
 import pytest  # noqa: F401
 
-from fastapi import APIRouter, HTTPException, status
-
 from uuid import uuid4
 
-from expenseflow.enums import FriendStatus
-from expenseflow.friend.models import UserModel, FriendModel
-from expenseflow.friend.schemas import FriendRead, UserRead
 from expenseflow.friend.service import create_accept_friend_request, \
-    get_sent_friend_requests, get_received_friend_requests, get_friends, \
-    remove_friend
+    get_sent_friend_requests
 from sqlalchemy.ext.asyncio import AsyncSession
 from httpx import AsyncClient
-from expenseflow.errors import ExpenseFlowError
 
 # remove later
-from expenseflow.user.service import get_user_by_id, get_user_by_nickname, get_all_users
+from expenseflow.user.service import get_user_by_nickname
 
 base_url = "/friends"
 
