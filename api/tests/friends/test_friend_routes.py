e88ee37ff793b287049de635957dc8c0c2baa3f0
Donghyug David Jeong <donghyug.jeong@student.uq.edu.au>
2025-06-03 10:46:09 +1000

    New: add by nickname

--- api/tests/friends/test_friend_routes.py
+++ api/tests/friends/test_friend_routes.py
@@ -2,6 +2,8 @@
 
 import pytest  # noqa: F401
 
+from fastapi import APIRouter, HTTPException, status
+
 from expenseflow.enums import FriendStatus
 from expenseflow.friend.models import UserModel, FriendModel
 from expenseflow.friend.schemas import FriendRead, UserRead
@@ -89,9 +91,56 @@ async def test_get_requests(session: AsyncSession,
     assert len(recv_response.json()) == 1
 
 @pytest.mark.asyncio
-async def test_create_by_nickname(test_client: AsyncClient, user_model_factory):
-    request = test_client.build_request(method="put",
-                                        url=base_url)
+async def test_create_by_nickname(session: AsyncSession,
+                                  test_client: AsyncClient,
+                                  user_model_factory,
+                                  default_user):
+    lucas_request = test_client.build_request(method="put",
+                                        url=base_url,
+                                        params={"db": session,
+                                                "user": default_user,
+                                                "nickname": "lucashicks1"})
+    david_request = test_client.build_request(method="put",
+                                        url=base_url,
+                                        params={"db": session,
+                                                "user": default_user,
+                                                "nickname": "daqoblade"})
+    tom_request = test_client.build_request(method="put",
+                                        url=base_url,
+                                        params={"db": session,
+                                                "user": default_user,
+                                                "nickname": "superstrooper"})
+    bad_request = test_client.build_request(method="put",
+                                        url=base_url,
+                                        params={"db": session,
+                                                "user": default_user,
+                                                "nickname": "faker"})
+    lucas = user_model_factory.build()
+    david = user_model_factory.build()
+    tom = user_model_factory.build()
+
+    lucas.nickname = "lucashicks1"
+    david.nickname = "daqoblade"
+    tom.nickname = "superstrooper"
+    
+    session.add(lucas)
+    session.add(david)
+    session.add(tom)
+    await session.commit()
+
+    lucas_response = await test_client.send(lucas_request)
+    david_response = await test_client.send(david_request)
+    tom_response = await test_client.send(tom_request)
+
+    friend_list = await get_sent_friend_requests(session, default_user)
+    assert len(friend_list) == 3
+
+    bad_response = await test_client.send(bad_request)
+
+    assert bad_response.json()['detail'] == \
+        "User under the nickname 'faker' could not be found"
+    
+    
 
 @pytest.mark.asyncio
 async def test_create(test_client: AsyncClient, user_model_factory):
